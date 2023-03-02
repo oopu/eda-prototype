d213cd41c525f5f5313534ec9037058fbdbc15d6
cblanco <nowucblanco@gmail.com>
2023-03-02 12:50:59 -0800

    Add to README and lint autotagging.py

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # eda-prototype
 A generic prototype for a primarily event-driven platform based on AWS using serverless technologies.
+
+# Features
+* An API gateway(v2) instance and stage with its URL and available routes exported (see `pulumi up` or `pulumi stack output` output)
+* An event bus with a single rule for a single Lambda function that echoes received messages from the given source to CloudWatch logs
+* Automatic resource tagging (for resources that support it) by domain, environment and Pulumi project ID
+
+# Taking it Further
+* You should consider adding a reusable component for creating an EventBridge Pipe that reads from SQS. This pattern will come up at least once in the future. See https://www.pulumi.com/docs/intro/concepts/resources/components/ for how to do so.
+* If you want to add infrastructure specific to another domain, like ecom's event bus and related functions, create a new Python package under something like /ecom and follow the same pattern under /ttc. Pulumi can attempt to import existing resources to the active stack with `pulumi import`.
+* Configure new stacks for your deployment environments with `pulumi init`
+* Add some tests to the packages with `pytest` and `behave` to get some CI going for your Lambda functions
--- utils/autotagging.py
+++ utils/autotagging.py
@@ -1,15 +1,22 @@
 import pulumi
 
+
 def register_auto_tags(auto_tags):
-    '''Registers a global stack transformation that merges a set of tags with any explicitly-added ones'''
-    pulumi.runtime.register_stack_transformation(lambda args: auto_tag(args, auto_tags))
+    '''
+    Registers a global stack transformation that merges a set of tags
+    with any explicitly-added ones
+    '''
+    pulumi.runtime.register_stack_transformation(
+        lambda args: auto_tag(args, auto_tags)
+    )
+
 
 def auto_tag(args, auto_tags):
     '''Applies the given tags to the resource properties if applicable.'''
-    # Note: this works with most AWS resources in Pulumi but autoscaling group definitions. This is a serverless 
-    # repo however, so this shouldn't come up.
-    # See https://www.pulumi.com/registry/packages/aws/api-docs/autoscaling/group/#tags_python for more
+    # This works with most AWS resources in Pulumi but autoscaling groups.
+    # This uses primarily serverless resources, so this shouldn't come up.
+    # See the following URL for more info:
+    # https://www.pulumi.com/registry/packages/aws/api-docs/autoscaling/group/#tags_python
     if hasattr(args.resource, "tags"):
         args.props['tags'] = {**(args.props['tags'] or {}), **auto_tags}
         return pulumi.ResourceTransformationResult(args.props, args.opts)
-    
